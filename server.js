a4ca76cb8ac1060960cc36fdc97791e149547d80
Nicholas Fiorentini <NFiorentini@GMail.com>
2016-10-11 22:54:59 -0700

    Correct error

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(function (req, res, next) {
 
 app.use(express.static('public'));
 
-app.listen(port, function () {
+app.listen(PORT, function () {
     console.log(
 `  Express server
 is listening & caring
