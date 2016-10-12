d13bde73a4670d2d7ac5f1d7b4a63bb304c359a0
Nicholas Fiorentini <NFiorentini@GMail.com>
2016-10-11 22:37:22 -0700

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
-
-// Create our app
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next) {
+    if(req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
+app.listen(port, function () {
     console.log(
 `  Express server
 is listening & caring
-  on port 3000!`)
+  on port ` + PORT + `!`);
 });
